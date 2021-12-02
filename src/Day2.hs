2ed832ecb376d658cf44cb0714a71501a97cdfdb
Quinn Wilson <github@midworld.org>
2021-12-02 00:07:41 -0600

    Day2 working

--- src/Day2.hs
+++ src/Day2.hs
@@ -2,11 +2,37 @@ module Day2
     ( 
     day2
    ,day2b
+   ,move
+   ,move2
     )
     where
-    
-day2 :: String -> Int 
-day2 input = 0 
 
+day2 :: String -> Int 
+day2 input = uncurry (*) $ foldr move (0,0) $ parseInput input
+
+move :: Num a => (String, a) -> (a, a) -> (a, a)
+move ("forward", forward) (x,y) = (x+forward, y)
+move ("up", up) (x,y) = (x, y - up)
+move ("down", down) (x,y) = (x, y + down)
+move (_,_) (_,_) = error "unexpected value"
+
+----
 day2b :: String -> Int
-day2b input = 0
+day2b input = (\(a,b,_)->a*b) $ foldr move2 (0,0,0) $ reverse $ parseInput input
+
+move2 :: Num a => (String, a) -> (a, a, a) -> (a, a, a)
+move2 ("forward", forward) (x,y,aim) = (x+forward, y + (forward*aim), aim)
+move2 ("up", up) (x,y,aim) = (x, y, aim - up)
+move2 ("down", down) (x,y,aim) = (x, y, aim + down)
+move2 (_,_) (_,_,_) = error "unexpected value"
+
+parseInput :: String -> [(String,Int)]
+parseInput input = map ((\[a,b]->(a,read b)).words)$ lines input
+
+_input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+
+{--
+parseInput' :: String -> [[Int]]
+parseInput' input = map (map read.words) $ lines input
+_input = "1 2 3 4\n5 6 7 8\n9 10 11 12\n13 14 15 16"
+--}
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -6,12 +6,34 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Day2" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+      let expected=150
       day2 input `shouldBe` expected
   describe "Day2b" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+      let expected=900
       day2b input `shouldBe` expected
+    describe "move" $ do
+      it "should follow basic command down" $ 
+        move ("down", 4) (1,1) `shouldBe` (1,5)
+      it "should follow basic command up" $ 
+        move ("up", 4) (1,1) `shouldBe` (1,-3)
+      it "should follow basic command forward" $ 
+        move ("forward", 4) (1,1) `shouldBe` (5,1)
+    describe "move2" $ do
+      it "should follow basic command forward" $ 
+        move2 ("forward", 5) (0,0,0) `shouldBe` (5,0,0)
+      it "should follow basic command down" $ 
+        move2 ("down", 5) (5,0,0) `shouldBe` (5,0,5)
+      it "should follow basic command forward" $ 
+        move2 ("forward", 8) (5,0,5) `shouldBe` (13,40,5)
+      it "up 3 decreases your aim by 3, resulting in a value of 2." $ 
+        move2 ("up", 3) (13, 40, 5) `shouldBe` (13, 40, 2)
+      it "down 8 adds 8 to your aim, resulting in a value of 10." $ 
+        move2 ("down", 8) (13, 40, 2) `shouldBe` (13, 40, 10)
+      it "forward 2 adds 2 to your horizontal position, a total of 15." $ 
+        move2 ("forward", 2) (13, 40, 10) `shouldBe` (15, 60, 10)
+
+
