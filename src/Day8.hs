8885b73b69f80248f489df76b7ef986a820d5d17
Quinn Wilson <github@midworld.org>
2021-12-08 18:12:35 -0600

    day8 done

--- src/Day8.hs
+++ src/Day8.hs
@@ -4,9 +4,66 @@ module Day8
    ,day8b
     )
     where
+import Data.List
+import Data.List.Split
+import Data.Maybe
     
 day8 :: String -> Int 
-day8 input = 0 
+day8 input = length 
+                $ concatMap catMaybes 
+                $ map (\u -> map lengthTest $ snd u) 
+                $ parseInput input
 
 day8b :: String -> Int
-day8b input = 0
+day8b input = sum $ map (\y-> read $ concatMap show y) $ map day8b' $ parseInput input
+
+test input =  (reference, map (\y-> (lookup (sort y))) $ concatMap snd rows)
+  where rows = parseInput input
+        lookup el = snd $ head $ filter (\(a,b)->a==el) reference
+        reference = zip (map sort $ day8b'' $ head $ parseInput input) [0..]
+
+day8b' input = map (\y->lookup $ sort y) $ snd input
+  where
+        lookup el = snd $ head $ filter (\(a,b)->a==el) reference
+        reference = zip (map sort $ day8b'' input) [0..]
+
+day8b'' (patterns, values) = [zero,one,two,three,four,five,six,seven,eight,nine]
+  where 
+    Just zero = find (\y->length y==6 &&  y/=nine && y/=six) patterns
+    Just one = find (\y->length y==2) patterns
+    Just two = find (\y->length y == 5 && length (eight \\ (nub $ four ++ y) ) == 0) patterns
+    Just three = find (\y->length y == 5 && length (y \\ one) == 3) patterns
+    Just four = find (\y->length y==4) patterns
+    Just five = find (\y->length y==5 && length (eight \\ (nub $ two ++ y)) ==0) patterns
+    Just six = find (\y->length y==6 && length (eight \\ nub (one++y) ) == 0) patterns
+    Just seven = find (\y->length y==3) patterns
+    Just eight = find (\y->length y==7) patterns
+    Just nine = find (\y->length y==6 && length (y \\ (nub $ four ++ five)) == 0) patterns
+  
+
+parseInput input = map ((\[a,b]->(words a,words b)). splitOn " | ") $ lines input
+
+lengthTest input
+  | length input == 2 = Just 1
+  | length input == 4 = Just 4
+  | length input == 3 = Just 7
+  | length input == 7 = Just 8
+  | otherwise = Nothing
+
+_input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce\n"
+_input2 = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+_input3 ="fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+
+_ref= [
+    ("abcdeg",0),
+    ("ab",1),
+    ("acdfg",2),
+    ("abcdf",3),
+    ("abef",4),
+    ("bcdef",5),
+    ("bcdefg",6),
+    ("abd",7),
+    ("abcdefg",8),
+    ("abcdef",9)
+  ]
+
--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -6,12 +6,52 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Day8" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce\n"
+      let expected=26
       day8 input `shouldBe` expected
   describe "Day8b" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+      let expected=5353
+      day8b input `shouldBe` expected
+    it "should do sample 2" $ do
+      let input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce\n"
+      let expected=61229
+      day8b input `shouldBe` expected
+    it "should do sample 1b" $ do
+      let input="be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+      let expected = 8394
+      day8b input `shouldBe` expected
+    it "should do sample 2b" $ do
+      let input="edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
+      let expected = 9781
+      day8b input `shouldBe` expected
+    it "should do sample 3b" $ do
+      let input="fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+      let expected = 9361
+      day8b input `shouldBe` expected
+    it "should do sample 4b" $ do
+      let input="aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
+      let expected = 4873
+      day8b input `shouldBe` expected
+    it "should do sample 5b" $ do
+      let input="fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
+      let expected = 8418
+      day8b input `shouldBe` expected
+    it "should do sample 5b" $ do
+      let input="dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
+      let expected = 4548
+      day8b input `shouldBe` expected
+    it "should do sample 5b" $ do
+      let input="bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
+      let expected = 1625
+      day8b input `shouldBe` expected
+    it "should do sample 5b" $ do
+      let input="egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
+      let expected = 8717
+      day8b input `shouldBe` expected
+    it "should do sample 5b" $ do
+      let input= "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+      let expected = 4315
       day8b input `shouldBe` expected
