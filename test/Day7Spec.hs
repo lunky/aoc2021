fc9fd7672247a9b4e52b1192235c1efdefcc0c11
Quinn Wilson <github@midworld.org>
2021-12-08 19:32:53 -0600

    refactor

--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -5,12 +5,12 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "Day7" $ do
+  describe "Day7" $ 
     it "should do sample 1" $ do
       let input = "16,1,2,0,4,2,7,1,2,14"
       let expected=37
       day7 input `shouldBe` expected
-  describe "Day7b" $ do
+  describe "Day7b" $ 
     it "should do sample 1" $ do
       let input = "16,1,2,0,4,2,7,1,2,14"
       let expected=168
